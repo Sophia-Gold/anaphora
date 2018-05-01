938d2bdd4491079ca16ba317965c9119182140fd
Sophia Gold <sophiagoldnyc@gmail.com>
2018-05-01 12:25:43 -0400

    use clojure fork w/ nested functions literals

--- project.clj
+++ project.clj
@@ -2,17 +2,18 @@
   :url "https://medium.com/@sophiagoldnyc/anaphora"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[local/clojure "1.9.999999"]
+                 [org.clojure/spec.alpha "0.1.143"]
+                 [org.clojure/core.specs.alpha "0.1.24"]
                  [org.clojure/data.avl "0.0.17"]
                  [org.clojure/data.int-map "0.2.4"]
-                 [org.clojure/math.combinatorics "0.1.4"]
-                 [com.rpl/specter "1.1.0"]
-                 [fipp "0.6.12"]
-                 [com.positronic-solutions/pulley.cps "0.2.2"]
+                 [org.clojure/math.combinatorics "0.1.4"] 
                  [org.clojure/tools.analyzer "0.6.9"]
                  [org.clojure/tools.analyzer.jvm "0.7.2"]
-                 [bronsa/tools.decompiler "0.1.0-alpha1"]] 
-  :plugins [[lein-nodisassemble "0.1.3"]] 
+                 [com.rpl/specter "1.1.0"]
+                 [fipp "0.6.12"]]
+  :exclusions [org.clojure/clojure]
+  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :main ^:skip-aot anaphora
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/anaphora.clj
+++ src/anaphora.clj
@@ -1,14 +1,10 @@
 (ns anaphora
   (:require [anaphora.chain :refer :all]
-           ;; [anaphora.church :as church] 
-            [clojure.string :as str]
+            [anaphora.church :as church]
             [fipp.edn :refer [pprint] :rename {pprint fipp}]
-            [com.positronic-solutions.pulley.cps :refer :all]
             [clojure.tools.analyzer :as ana]
             [clojure.tools.analyzer.jvm :refer [analyze]]
-            [clojure.tools.analyzer.env :as env]
-            ;; [clojure.tools.decompiler :refer :all]))
-            [no.disassemble :refer :all]))
+            [clojure.tools.analyzer.env :as env]))
 
 (defn chain
   [f g order]
@@ -96,51 +92,30 @@
          (sequence (partial xf3 (partial xf2 xf1)))
          (apply add)))) 
 
-;; (defn chain4
-;;   "only works with fork of Clojure allowing nested literals" 
-;;   [f g order]
-;;   (let [f (nth (iterate add-dim f)
-;;                (dec (long (count (ffirst g)))))
-;;         f' (diff-unmixed1 f order 1)
-;;         g' (diff g order)] 
-;;     (->> order
-;;          partition-set
-;;          (map #(mul (multi-compose (nth f' (dec (count %))) g)
-;;                     (->> %
-;;                          (map #(->> %
-;;                                     (map-indexed #(*' (long (Math/pow 10 %1)) 
-;;                                                       %2))
-;;                                     (reduce +')
-;;                                     (get g')))
-;;                          (apply mul)))
-;;               (apply add)))))
+(defn chain4
+  "only works with fork of Clojure allowing nested literals" 
+  [f g order]
+  (let [f (nth (iterate add-dim f)
+               (dec (long (count (ffirst g)))))
+        f' (diff-unmixed1 f order 1)
+        g' (diff g order)] 
+    (->> order
+         partition-set
+         (map #(mul (multi-compose (nth f' (dec (count %))) g)
+                    (->> %
+                         (map #(->> %
+                                    (map-indexed #(*' (long (Math/pow 10 %1)) 
+                                                      %2))
+                                    (reduce +')
+                                    (get g')))
+                         (apply mul)))
+              (apply add)))))
  
 (defn chain-test []
   (= (chain3 {[2] 1, [1] 5, [0] 7}
              {[1 1] 2, [1 0] 3, [0 1] 5, [0 0] 7}
              2)
      {[1 1] 16, [1 0] 24, [0 1] 40, [0 0] 68}))
-
-(defn stacks []
-  (->> disassemble
-       call-cc
-       str/split-lines 
-       (filter #(re-find #"Stack" %)) 
-       (map (comp (fn [s] (map #(Long/parseLong (re-find #"\d+" %)) s))
-                  #(str/split % #",")))
-       (map #(zipmap [:stack :locals] %))
-       (sort-by :stack)
-       fipp))
-
-(defn get-cc []
-  (call-cc 
-   (fn [cc]
-     #(cc (conj [] %)))))
-
-(defn read-cc []
-  (-> (get-cc)
-      disassemble-data 
-      fipp))
 
 (defn -main []
   )
