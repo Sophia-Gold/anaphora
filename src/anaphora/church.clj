a3b48b07301e6afef1118463c70161e2b55a8116
Sophia Gold <sophiagoldnyc@gmail.com>
2018-05-20 03:15:18 -0400

    printing

--- src/anaphora/church.clj
+++ src/anaphora/church.clj
@@ -1,13 +1,7 @@
 (ns anaphora.church
   (:require [anaphora.util :refer :all]))
 
-(defn church [n]
-  (letfn [(succ [n] (fn [f] (fn [x] (f ((n f) x)))))]
-    (loop [i 0
-           ret (fn [f] (fn [x] x))]
-      (if (= i n)
-        ret
-        (recur (inc i) (succ ret))))))
+(def print-church (fn [f] ((f (fn [n] (format "f(%s)" n))) "n")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -15,35 +9,35 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmacro zero  [] '(fn [f] (fn [x] x)))
-(defmacro one   [] '(fn [f] (fn [x] (f x))))
-(defmacro two   [] '(fn [f] (fn [x] (f (f x)))))
-(defmacro three [] '(fn [f] (fn [x] (f (f (f x))))))
-(defmacro four  [] '(fn [f] (fn [x] (f (f (f (f x)))))))
-(defmacro five  [] '(fn [f] (fn [x] (f (f (f (f (f x))))))))
-(defmacro six   [] '(fn [f] (fn [x] (f (f (f (f (f (f x)))))))))
-(defmacro seven [] '(fn [f] (fn [x] (f (f (f (f (f (f (f x))))))))))
-(defmacro eight [] '(fn [f] (fn [x] (f (f (f (f (f (f (f (f x)))))))))))
-(defmacro nine  [] '(fn [f] (fn [x] (f (f (f (f (f (f (f (f (f x))))))))))))
+(def zero  (fn [f] (fn [x] x)))
+(def one   (fn [f] (fn [x] (f x))))
+(def two   (fn [f] (fn [x] (f (f x)))))
+(def three (fn [f] (fn [x] (f (f (f x))))))
+(def four  (fn [f] (fn [x] (f (f (f (f x)))))))
+(def five  (fn [f] (fn [x] (f (f (f (f (f x))))))))
+(def six   (fn [f] (fn [x] (f (f (f (f (f (f x)))))))))
+(def seven (fn [f] (fn [x] (f (f (f (f (f (f (f x))))))))))
+(def eight (fn [f] (fn [x] (f (f (f (f (f (f (f (f x)))))))))))
+(def nine  (fn [f] (fn [x] (f (f (f (f (f (f (f (f (f x))))))))))))
 
-(defmacro succ [n]   '(fn [f] (fn [x] (f (~n f) x))))
-(defmacro add  [m n] '(fn [f] (fn [x] ((~m f) ((~n f) x)))))
-(defmacro mult [m n] '(fn [f] (fn [x] ((~m (~n f)) x))))
-(defmacro pow  [m n] '(fn [f] (fn [x] (((~n ~m) f) x))))
-(defmacro pred   [n] '(fn [f] (fn [x] (((~n (fn [g] (fn [h] (h (g f))))) (fn [u] x)) (fn [u] u)))))
-(defmacro sub  [m n] '(fn [f] (fn [x] ((((~n pred) ~m) f) x))))
+(def succ (fn [n] (fn [f] (fn [x] (f ((n f) x))))))
+(def add  (fn [m n] (fn [f] (fn [x] ((m f) ((n f) x))))))
+(def mul  (fn [m n] (fn [f] (fn [x] ((m (n f)) x)))))
+(def pow  (fn [m n] (fn [f] (fn [x] (((n m) f) x)))))
+(def pred (fn   [n] (fn [f] (fn [x] (((n (fn [g] (fn [h] (h (g f))))) (fn [u] x)) (fn [u] u))))))
+(def sub  (fn [m n] (fn [f] (fn [x] ((((n pred) m) f) x)))))
 
-(defmacro ctrue   []    '(fn [a] (fn [b] a)))
-(defmacro cfalse  []    '(fn [a] (fn [b] b)))
-(defmacro cand [p q]   '(~p (~q ~p)))
-(defmacro cor  [p q]   '(~p (~p ~q)))
-(defmacro cnot [p]     '(~p (cfalse (ctrue))))
-(defmacro cxor [a b]   '(~a (cnot ~b) ~b))
-(defmacro cif  [p a b] '(~p ~a ~b))
+(def ctrue  (fn [a] (fn [b] a)))
+(def cfalse (fn [a] (fn [b] b)))
+(def cand   (fn [p q] (p (q p))))
+(def cor    (fn [p q]   (p (p q))))
+(def cnot   (fn [p]     (p (cfalse (ctrue)))))
+(def cxor   (fn [a b]   (a (cnot b) b)))
+(def cif    (fn [p a b] (p a b)))
 
-(defmacro czero? [n]   '(~n (fn [x] cfalse) ctrue))
-(defmacro leq?   [m n] '(czero? (minus ~m ~n)))
-(defmacro eq?    [m n] '(cand (leq ~m ~n) (leq ~n ~m)))
+(def czero? (fn [n] (n (fn [x] cfalse) ctrue)))
+(def leq?   (fn [m n] (czero? (sub m n))))
+(def eq?    (fn [m n] (cand (leq? m n) (leq? n m))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -51,26 +45,26 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmacro zero'  [] '(fn [f] #(%)))
-(defmacro one'   [] '(fn [f] #(f %)))
-(defmacro two'   [] '(fn [f] #(f (f %))))
-(defmacro three' [] '(fn [f] #(f (f (f %)))))
-(defmacro four'  [] '(fn [f] #(f (f (f (f %))))))
-(defmacro five'  [] '(fn [f] #(f (f (f (f (f %)))))))
-(defmacro six'   [] '(fn [f] #(f (f (f (f (f (f %))))))))
-(defmacro seven' [] '(fn [f] #(f (f (f (f (f (f (f %)))))))))
-(defmacro eight' [] '(fn [f] #(f (f (f (f (f (f (f (f %))))))))))
-(defmacro nine'  [] '(fn [f] #(f (f (f (f (f (f (f (f (f %)))))))))))
+(def zero'   (fn [f] #(%)))
+(def one'    (fn [f] #(f %)))
+(def two'    (fn [f] #(f (f %))))
+(def three'  (fn [f] #(f (f (f %)))))
+(def four'   (fn [f] #(f (f (f (f %))))))
+(def five'   (fn [f] #(f (f (f (f (f %)))))))
+(def six'    (fn [f] #(f (f (f (f (f (f %))))))))
+(def seven'  (fn [f] #(f (f (f (f (f (f (f %)))))))))
+(def eight'  (fn [f] #(f (f (f (f (f (f (f (f %))))))))))
+(def nine'   (fn [f] #(f (f (f (f (f (f (f (f (f %)))))))))))
 
-(defmacro succ'   [n] '(fn [f] #(f ((~n f) %))))
-(defmacro add'  [m n] '(fn [f] #((~m f) ((~n f) %))))
-(defmacro mult' [m n] '(fn [f] #((~m (~n f)) %)))
-(defmacro pow'  [m n] '(fn [f] #(((~n ~m) f) %)))
-(defmacro pred'   [n] '(fn [f] (#((~n (fn [g] (fn [h] (h (g f))))) %)) #(%)))
-(defmacro sub'  [m n] '(fn [f] #((((~n pred) ~m) f) %)))
+(def succ' (fn [n] (fn [f] #(f ((n f) %)))))
+(def add'  (fn [m n] (fn [f] #((m f) ((n f) %)))))
+(def mul'  (fn [m n] (fn [f] #((m (n f)) %))))
+(def pow'  (fn [m n] (fn [f] #(((n m) f) %))))
+(def pred' (fn [n] (fn [f] (#((n (fn [g] (fn [h] (h (g f))))) %)) #(%))))
+(def sub'  (fn [m n] (fn [f] #((((n pred) m) f) %))))
 
-(defmacro ctrue'  []  '(fn [a] #(a)))
-(defmacro cfalse' []  '(fn [a] #(%)))
+(def ctrue'  (fn [a] #(a)))
+(def cfalse' (fn [a] #(%)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -78,23 +72,23 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmacro zero''  [] '#(%))
-(defmacro one''   [] '#(%1 %2))
-(defmacro two''   [] '#(%1 (%1 %2)))
-(defmacro three'' [] '#(%1 (%1 (%1 %2))))
-(defmacro four''  [] '#(%1 (%1 (%1 (%1 %2)))))
-(defmacro five''  [] '#(%1 (%1 (%1 (%1 (%1 %2))))))
-(defmacro six''   [] '#(%1 (%1 (%1 (%1 (%1 (%1 %2)))))))
-(defmacro seven'' [] '#(%1 (%1 (%1 (%1 (%1 (%1 (%1 %2))))))))
-(defmacro eight'' [] '#(%1 (%1 (%1 (%1 (%1 (%1 (%1 (%1 %2)))))))))
-(defmacro nine''  [] '#(%1 (%1 (%1 (%1 (%1 (%1 (%1 (%1 (%1 %2))))))))))
+(def zero'   #(%))
+(def one'    #(%1 %2))
+(def two'    #(%1 (%1 %2)))
+(def three'  #(%1 (%1 (%1 %2))))
+(def four'   #(%1 (%1 (%1 (%1 %2)))))
+(def five'   #(%1 (%1 (%1 (%1 (%1 %2))))))
+(def six'    #(%1 (%1 (%1 (%1 (%1 (%1 %2)))))))
+(def seven'  #(%1 (%1 (%1 (%1 (%1 (%1 (%1 %2))))))))
+(def eight'  #(%1 (%1 (%1 (%1 (%1 (%1 (%1 (%1 %2)))))))))
+(def nine'   #(%1 (%1 (%1 (%1 (%1 (%1 (%1 (%1 (%1 %2))))))))))
 
-(defmacro succ''   [n] '#(%1 ((~n %1) %2)))
-(defmacro add''  [m n] '#((~m %1) ((~n %1) %2)))
-(defmacro mult'' [m n] '#((~m (~n %1)) %2))
-(defmacro pow''  [m n] '#(((~n ~m) %1) %2))
-(defmacro pred'' [n]   '#(((~n (%4 (%3 %1))) %2) (%5)))
-(defmacro sub''  [m n] '#((((~n pred) ~m) %1) %2))
+(def succ' #(%2 ((%1 %2) %2)))
+(def add'  #((%2 %3) ((%1 %3) %4)))
+(def mul'  #((%1 (%2 %3)) %4))
+(def pow'  #(((%1 %1) %3) %4))
+(def pred' #(((%1 (%5 (%4 %2))) %3) (%6)))
+(def sub'  #((((%2 pred) %1) %3) %4))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -102,7 +96,7 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; (defmacro pred [n] '#(#((~n #(% #(% %))) %) %))
+(def pred-literal #(#(#(% #(% #(% %))) %) %))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -120,9 +114,8 @@
 
 (defn church-factorial [n]
   (loop [cnt n
-         acc '(one)]
-    (expand
-     '(cif (expand '(czero? (cnt)))
-           (expand '(acc))
-           (recur '(pred (cnt))
-                  (expand '(mul (cnt) (acc))))))))
+         acc one]
+    (cif (print-church (czero? cnt))
+         (print-church acc)
+         (church-factorial (pred cnt)
+                           (mul cnt acc)))))
