705cfda14332ce732cfd0ea01a9cc19c0fe9dab3
Sophia Gold <sophiagoldnyc@gmail.com>
2018-05-23 03:44:45 -0400

    late night macrology pt2

--- test/anaphora/test.clj
+++ test/anaphora/test.clj
@@ -1,6 +1,7 @@
 (ns anaphora.test
   (:require [clojure.test :refer :all]
-            [anaphora :refer :all]))
+            [anaphora :refer :all]
+            [anaphora.church :refer :all]))
 
 (deftest chain-test
   (is (= (chain {[2] 1, [1] 5, [0] 7}
@@ -37,3 +38,27 @@
                  {[1 1] 2, [1 0] 3, [0 1] 5, [0 0] 7}
                  2)
          {[1 1] 16, [1 0] 24, [0 1] 40, [0 0] 68})))
+
+(deftest succ-test
+  (= (cstr (succ (succ zero)))
+     (cstr two)))
+
+;; (deftest succ2-test
+;;   (= (cstr (succ' (succ' zero')))
+;;      (cstr two')))
+
+(deftest succ3-test
+  (= (cstr (succ'' (succ'' zero'')))
+     (cstr two'')))
+
+(deftest pred-test
+  (= (cstr (pred (pred nine)))
+     (cstr seven)))
+
+;; (deftest pred2-test
+;;   (= (cstr (pred' (pred' nine')))
+;;      (cstr seven')))
+
+(deftest pred3-test
+  (= (cstr (pred'' (pred'' nine'')))
+     (cstr seven'')))
